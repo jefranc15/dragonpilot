a9aadf95773e8419a28443d03d35698ffbd2aaf2
jefranc15 <johneduardfranco@gmail.com>
2026-09-12 02:51:48 +0800

    panda: start LG-to-NEOS handoff watcher on EON

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -7,5 +7,12 @@ if [ $size -le 1024 ];then
 fi
 
 export PASSIVE="0"
+
+# OnePlus/NEOS: take over a Black Panda left running LG G8 firmware.
+# The service only touches 3801:ddcc and hands STM32 DFU back to normal pandad.
+if [ -f /EON ]; then
+    nohup python -u ./scripts/panda_neos_handoff.py >> /data/panda_neos_handoff.log 2>&1 &
+fi
+
 exec ./launch_chffrplus.sh
 
